131a5848f0b7dad4d176c294841646adb9f9c987
Unidarkshin <devinlematty@gmail.com>
2018-05-21 19:44:52 -0400

    Updates
    API

--- Mods/LevelExtender/ModEntry.cs
+++ Mods/LevelExtender/ModEntry.cs
@@ -929,10 +929,6 @@ namespace LevelExtender
             //This value will offset spawn-rate by the specified amount (1 second intervals)
             public double overSR = -1.0;
 
-            public void spawn_rate(double osr)
-            {
-                overSR = osr;
-            }
         }
     }
 }
